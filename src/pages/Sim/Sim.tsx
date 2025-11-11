54ca3dd888960233b2c21d566e2043cd2dec0e20
Sean Hetzel <seanhetzel1@gmail.com>
2025-11-10 17:56:00 -0700

    add home button

--- src/pages/Sim/Sim.tsx
+++ src/pages/Sim/Sim.tsx
@@ -1,14 +1,29 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import kiwaImage from "../../assets/airports/KIWA.png";
 import styles from "./Sim.module.css";
+import { IconButton } from "@mui/joy";
+import HomeIcon from "@mui/icons-material/Home";
 
 export default function Sim() {
   const [searchParams] = useSearchParams();
   const scenario = searchParams.get("scenario");
+  const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
-      <p>Selected Scenario: {scenario}</p>
+      <div className={styles.simHUD}>
+        <IconButton
+          onClick={() => {
+            navigate("/");
+          }}
+          className={styles.homeButton}
+          variant="plain"
+          color="primary"
+        >
+          <HomeIcon />
+        </IconButton>
+        <p>Selected Scenario: {scenario}</p>
+      </div>
       <img
         src={kiwaImage}
         alt="Phoenix-Mesa Gateway Airport (KIWA)"
